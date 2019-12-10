4926fc3e895e6358ffcede07fc1ce4546291e9a7
Vukasin <vule993@outlook.com>
2019-12-11 00:29:42 +0100

    fix

--- Projekat/Contracts/IWCFService.cs
+++ Projekat/Contracts/IWCFService.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Contracts
 {
-    class IWCFService
+    [ServiceContract]
+    public interface IWCFService
     {
+
+        [OperationContract]
+        double AverageSalaryByCityAndAge(string city, short fromAge, short toAge);
+        [OperationContract]
+        double AverageSalaryByCountryAndPayday(string country, string payDay);
+        [OperationContract]
+        bool CreateDatabase(string filename);
+        [OperationContract]
+        bool DeleteDatabase(string filename);
+        [OperationContract]
+        bool Edit(int id, string country, string city, short age, double salary, string payDay);
+        [OperationContract]
+        bool Insert(string country, string city, short age, double salary, string payDay);
+        [OperationContract]
+        string ViewAll();
+        [OperationContract]
+        string ViewMaxPayed(bool tf);
     }
 }
