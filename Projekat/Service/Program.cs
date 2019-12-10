0c9b9f61d7dc4cc25555e07f4a8b0555472a6a37
Dexter <34897084+Dexter255@users.noreply.github.com>
2019-12-10 22:49:13 +0100

    WCFService added

--- Projekat/Service/Program.cs
+++ Projekat/Service/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,17 @@ namespace Service
     {
         static void Main(string[] args)
         {
+            NetTcpBinding binding = new NetTcpBinding();
+            string address = "net.tcp://localhost:9999/WCFService";
+
+            ServiceHost serviceHost = new ServiceHost(typeof(WCFService));
+            serviceHost.AddServiceEndpoint(typeof(IWCFService), binding, address);
+
+            serviceHost.Open();
+            Console.WriteLine("WCFService is opened. Press <enter> to finish...");
+            Console.ReadLine();
+
+            serviceHost.Close();
         }
     }
 }
