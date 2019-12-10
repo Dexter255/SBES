7b07d9e58d76d767844073b39b5ae1cf671dbbf3
Dexter <34897084+Dexter255@users.noreply.github.com>
2019-12-10 22:59:35 +0100

    WCFService implemented IWCFService

--- Projekat/Service/WCFService.cs
+++ Projekat/Service/WCFService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Contracts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,54 @@ using System.Threading.Tasks;
 
 namespace Service
 {
-    public class WCFService
+    public class WCFService : IWCFService
     {
+        public bool CreateDatabase(string filename)
+        {
+            Console.WriteLine("Create database succeeded");
+            return true;
+        }
+
+        public bool DeleteDatabase(string filename)
+        {
+            Console.WriteLine("Delete database succeeded");
+            return true;
+        }
+
+        public bool Edit(int id, string country, string city, short age, double salary, string payDay)
+        {
+            Console.WriteLine("Edit succeeded");
+            return true;
+        }
+
+        public bool Insert(string country, string city, short age, double salary, string payDay)
+        {
+            Console.WriteLine("Insert succeeded");
+            return true;
+        }
+
+        public bool View()
+        {
+            Console.WriteLine("View1 succeeded");
+            return true;
+        }
+
+        public bool View(bool tf)
+        {
+            Console.WriteLine("View2 succeeded");
+            return true;
+        }
+
+        public bool View(string city, short fromAge, short toAge)
+        {
+            Console.WriteLine("View3 succeeded");
+            return true;
+        }
+
+        public bool View(string country, string payDay)
+        {
+            Console.WriteLine("View4 succeeded");
+            return true;
+        }
     }
 }
