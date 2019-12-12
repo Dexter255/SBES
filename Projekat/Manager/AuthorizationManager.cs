e26a483bfc59ddc4877dfc6de8bc64c8fde95008
Dexter <34897084+Dexter255@users.noreply.github.com>
2019-12-12 12:04:26 +0100

    RBAC Authorization

---
+++ Projekat/Manager/AuthorizationManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manager
+{
+    public class AuthorizationManager : ServiceAuthorizationManager
+    {
+        // poziva se pre poziva metode u servisu kako bi se obezbedila provera prava pirstupa korisnika koji je pozvao metodu
+        // posto se poziva pre metode u servisu, nit jos njije kreirana, tako da iz niti nije moguce izvaditi user-a koji
+        // je pozvao metodu
+        protected override bool CheckAccessCore(OperationContext operationContext)
+        {
+            return base.CheckAccessCore(operationContext);
+        }
+    }
+}
--- Projekat/Service/Program.cs
+++ Projekat/Service/Program.cs
@@ -1,8 +1,11 @@
 ﻿using Contracts;
+using Manager;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Policy;
 using System.Linq;
 using System.ServiceModel;
+using System.ServiceModel.Description;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,6 +20,15 @@ namespace Service
 
             ServiceHost serviceHost = new ServiceHost(typeof(WCFService));
             serviceHost.AddServiceEndpoint(typeof(IWCFService), binding, address);
+
+            serviceHost.Description.Behaviors.Remove(typeof(ServiceDebugBehavior));
+            serviceHost.Description.Behaviors.Add(new ServiceDebugBehavior() { IncludeExceptionDetailInFaults = true });
+
+            //serviceHost.Authorization.ServiceAuthorizationManager = new AuthorizationManager();
+            serviceHost.Authorization.PrincipalPermissionMode = PrincipalPermissionMode.Custom;
+            List<IAuthorizationPolicy> policies = new List<IAuthorizationPolicy>();
+            policies.Add(new CustomPolicy());
+            serviceHost.Authorization.ExternalAuthorizationPolicies = policies.AsReadOnly();
 
             serviceHost.Open();
             Console.WriteLine("WCFService is opened. Press <enter> to finish...");
